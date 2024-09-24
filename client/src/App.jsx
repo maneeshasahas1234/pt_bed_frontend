4436ade081f8233705d184844df0099979cf2eea
maneeshasahas1234 <116358625+maneeshasahas1234@users.noreply.github.com>
2024-09-24 22:32:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import Dashboard from "../pages/Dashboard.jsx";
 import axios from "axios";
 
-axios.defaults.baseURL = "167.99.8.130:8888";
+axios.defaults.baseURL = "http://167.99.8.130:8888/";
 
 function App() {
 
